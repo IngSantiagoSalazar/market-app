bd1a3722058aa4f9972b1aa1010dc0ae7a3f110a
Juan C <jcdiaz.9291@unicesmag.edu.co>
2025-09-09 15:38:47 -0500

    aja2

---
+++ src/signup.php
@@ -0,0 +1,28 @@
+<?php
+//Step 1 get database access
+
+require('../config/database.php');
+//Step 2 get form-data
+
+$f_name = $_POST['fname']; //los campos dentro de los corchetes 
+$l_name = $_POST['lname']; //deber ser igual al de el $
+$m_number = $_POST['mnumber'];
+$ide_number = $_POST['idnumber'];
+$e_mail = $_POST['email'];
+$p_wd = $_POST['passwd'];
+
+//Step 3 create query to insert into
+$query = "INSERT INTO users(firstname,lastname,mobile_number,ide_number,email,password)
+Values('$f_name','$l_name','$m_number','$ide_number','$e_mail','$p_wd')";
+
+//Step 4  execute query
+ $res= pg_query($conn,$query);
+
+ //Step 5 validate result
+ if($res){
+  echo "User has been created succesfully!!!";  
+ }else{
+    echo "something wrong !";
+ }
+
+?>
